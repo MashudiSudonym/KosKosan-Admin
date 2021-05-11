04ace618206c5c913c3657a3cc9643327b72d781
masrobot <muhamadmashudiardiwinata@gmail.com>
2021-05-11 21:25:52 +0700

    add repository module and ViewModel module at Module.kt

--- app/src/main/java/c/m/koskosanadmin/di/Module.kt
+++ app/src/main/java/c/m/koskosanadmin/di/Module.kt
@@ -1,8 +1,17 @@
 package c.m.koskosanadmin.di
 
+import c.m.koskosanadmin.data.repository.AuthRepository
+import c.m.koskosanadmin.data.repository.FirebaseRepository
+import c.m.koskosanadmin.ui.splash.SplashScreenViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-val viewModelModule: Module = module {  }
+val viewModelModule: Module = module {
+    viewModel { SplashScreenViewModel(get()) }
+}
 
-val repositoryModule: Module = module {  }
+val repositoryModule: Module = module {
+    single { AuthRepository() }
+    single { FirebaseRepository() }
+}
