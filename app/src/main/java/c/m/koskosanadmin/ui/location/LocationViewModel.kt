eea10e8f0899ecb5a065dd295e6785fc7c22b145
masrobot <muhamadmashudiardiwinata@gmail.com>
2021-05-12 12:15:33 +0700

    rename name of bottom navigation menu

--- app/src/main/java/c/m/koskosanadmin/ui/location/LocationViewModel.kt
+++ app/src/main/java/c/m/koskosanadmin/ui/location/LocationViewModel.kt
@@ -1,13 +1,13 @@
-package c.m.koskosanadmin.ui.notifications
+package c.m.koskosanadmin.ui.location
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class LocationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/c/m/koskosanadmin/ui/transaction/TransactionViewModel.kt
+++ app/src/main/java/c/m/koskosanadmin/ui/transaction/TransactionViewModel.kt
@@ -1,10 +1,10 @@
-package c.m.koskosanadmin.ui.home
+package c.m.koskosanadmin.ui.transaction
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class TransactionViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
