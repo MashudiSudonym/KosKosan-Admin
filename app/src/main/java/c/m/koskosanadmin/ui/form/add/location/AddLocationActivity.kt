b4047300e9819dfb8308399804f5a989eab3f55b
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2021-06-03 22:53:47 +0700

    add new activity AddLocationActivity.kt

---
+++ app/src/main/java/c/m/koskosanadmin/ui/form/add/location/AddLocationActivity.kt
@@ -0,0 +1,12 @@
+package c.m.koskosanadmin.ui.form.add.location
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import c.m.koskosanadmin.R
+
+class AddLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_location)
+    }
+}
